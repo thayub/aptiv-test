ac0a4e6ac523f34f2405455e5390cc0be3344f8a
blue <thayub@gmail.com>
2020-05-04 23:40:05 +0800

    fix: Joi validation

--- app/controllers/bookingContoller.js
+++ app/controllers/bookingContoller.js
@@ -1,6 +1,6 @@
 const Joi = require("@hapi/joi");
 const helperFn = require('../../app/helper/default');
-
+const errorMessage = require('http-errors');
 
 
 const schema = Joi.object().keys({
@@ -9,19 +9,23 @@ const schema = Joi.object().keys({
 });
 
 const createBooking =  (req, res, next) => {
-    console.log(req.body);
-    console.log(req.body.destination);
+    console.log("iInside the controller");
     const result = schema.validate(req.body);
     console.log("=-=-=-=");
     console.log(result.error);
-    if (result.error !== null || undefined){
-        // next(createError(400, 'The input is malformed, kindly check the structure'));
-        res.status(422).json({
+    if (result.error !== (null || undefined)){
+        res.status(400).json({
             message: 'The input is malformed, kindly check the structure'
+        })
+    }else{
+        res.status(200).json({
+            message: 'proper input'
         })
     }
     helperFn.getEmptyCars().then((availableCars) => {
         // finding the car which is available immediately
+        console.log('availableCars');
+        console.log(availableCars);
         // Updating the value after finalizing a car
     }).catch(err => next(err));
 }
